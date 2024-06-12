35aa6a1b6617036889a435ac9f9b3abbbe22fc35
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-06-12 14:26:11 -0500

    small change to php to test

--- forms/contact.php
+++ forms/contact.php
@@ -2,7 +2,7 @@
 
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'rickeynaquin@cleanslatepressurewashingnola.com';
+  $receiving_email_address = 'alexrayer7@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -30,6 +30,7 @@
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['phone'], 'Phone');
+  $contact->add_message( $_POST['subject'], 'Service Interested in');
   $contact->add_message( $_POST['message'], 'Message');
   
 
